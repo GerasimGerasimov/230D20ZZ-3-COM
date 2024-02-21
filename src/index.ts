54df189e44bd14746d86531a74f7be3fdec15b58
GerasimGerasimov <dialix@yandex.ru>
2024-02-21 18:03:59 +0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import fs = require ("fs");
+import ComPort from "./comport/comport";
+import { appendCRC16toArray, getCRC16 } from "./crc/crc16";
+import { Settings } from "./utils/config";
+import { delay } from "./utils/delay";
+
+console.log('Start 230D20ZZ-3');
+
+const COMx: ComPort = new ComPort(Settings.COM);
+
+(async () => { 
+  while (true) {
+    try {
+      await COMx.waitForOpen();
+      const msg:Array<any> = await sendCmd([0x01, 0x03, 0x00,0x00, 0x00,0x01]);
+      console.log(msg);
+      process.exit(0);
+    } catch (e) {
+      await delay(1000);
+      console.log('Исключение',e);
+    }
+  }
+})();
+
+async function sendCmd(command:Array<any>): Promise<any> {
+  const cmdSource = new Uint8Array([...command]);
+  const cmd: Array<number> = Array.from(appendCRC16toArray(cmdSource));
+  const answer: any = await COMx.getCOMAnswer({cmd, 
+                                               ChunksEndTime:100,
+                                               timeOut:3800});
+  const msg: Array<number> = validateAnswer(answer);
+  return msg;
+}
+
+function validateAnswer(answer: any): Array<number> | never {
+  if (!('msg' in answer)) throw new Error(`The Answer has no 'msg' field`);
+  if (getCRC16(answer.msg) != 0) throw new Error(`The Answer CRC doesn't match`);
+  return answer.msg;
+}
